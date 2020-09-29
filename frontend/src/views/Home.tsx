b19e8a2cd58099f9f6cd1b40a274fc01fb78f358
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-29 17:12:25 -0500

    Created css card and rendered content to populate their own card

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { RouteProps } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 
 import styles from '../styles/home.module.css'
 import Image from '../images/Concert_Logo .png'
+
 import RockImage from '../images/rock.png'
-import Rock2Image from '../images/rock2.png'
-import HipHopImage from '../images/hiphop.png'
-import PopImage from '../images/pop.png'
+import client from '../client/index'
+
+import { Concert } from '../interfaces/concerts'
 
 
-const Home = () => {
+const Home = ({ routeProps }: Props) => {
+  console.log('route props', routeProps)
+  const [concerts, setConcerts] = useState<Concert[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+
+      const fetchConcerts = async (): Promise<void> => {
+          try {
+            const { data } = (await client.get('/concerts')) as ConcertApiResponse
+
+            setConcerts(data)
+          } catch (error) {
+            console.error('error', error)
+
+          } finally {
+            setIsLoading(false)
+          }
+        }
+        fetchConcerts()
+      }, [])
+
   return (
     <div className={styles.landing}>
        <div className={styles.MainHeader} >
@@ -21,25 +43,40 @@ const Home = () => {
           </div>
     </div>
 
-    <div className={styles.GridContainer}>
-        <div className={styles.aboutUs}>
-          <h1>The One and Only Place</h1>
-          {/* <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus ex similique, dolorum minima autem veritatis tempore ea qui alias, et nisi fuga necessitatibus in cumque quisquam soluta accusantium rerum maxime impedit quaerat delectus. Illo nobis fugit delectus sint reprehenderit a ad quaerat cum, minima commodi suscipit adipisci veniam et laboriosam blanditiis ipsa, natus cupiditate temporibus deleniti consectetur distinctio. Adipisci qui dignissimos necessitatibus inventore, placeat accusamus debitis praesentium impedit nostrum amet natus, exercitationem, quos modi maxime excepturi? Natus necessitatibus voluptates quia nobis dicta, quas libero vitae sint sequi illo voluptatum nesciunt, quod sunt corrupti! Itaque, esse natus! Fugiat enim nam sunt.</p> */}
+    <div className={styles.Card_Container}>
+
+    {concerts.map(concert => (
+        <div className={styles.card}>
+          <div className={styles.card_header}>
+            <img className={styles.card_image} src={RockImage} alt=""/>
+          </div>
+          <div className={styles.card_body}>
+
+                    <tr key={concert.id}>
+                      <td>{concert.artist}</td>
+                      <td>{concert.location}</td>
+                      <td>{concert.date}</td>
+                    </tr>
+
+          </div>
         </div>
-        <div>
-          <h1>One Click away!</h1>
-        </div>
-        <div className={styles.nested_grid}>
-          <img className={styles.nested_grid_image} src={RockImage} alt=""/>
-          <img className={styles.nested_grid_image} src={HipHopImage} alt=""/>
-          <img className={styles.nested_grid_image} src={PopImage} alt=""/>
-          <img className={styles.nested_grid_image} src={Rock2Image} alt=""/>
-        </div>
-     </div>
+
+        ))}
+      </div>
     </div>
   )
 }
+  interface Props {
+    routeProps: RouteProps
+  }
+  interface ConcertApiResponse {
+    data: Concert[]
+  }
 
 export default Home
 
 
+ {/* <img className={styles.nested_grid_image} src={RockImage} alt=""/>
+          <img className={styles.nested_grid_image} src={HipHopImage} alt=""/>
+          <img className={styles.nested_grid_image} src={PopImage} alt=""/>
+          <img className={styles.nested_grid_image} src={Rock2Image} alt=""/> */}
